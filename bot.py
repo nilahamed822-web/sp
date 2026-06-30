fc6fbdbef573640f92bbb87f899fef475c8aa37d
Md Limon <nilahamed822@gmail.com>
2026-06-30 15:31:27 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from faker import Faker
 
 API_ID = 39283277
 API_HASH = '56cd66f3f3fd7da78db44263ea05e0dc'
-BOT_TOKEN = '8462558990:AAFAYEXqn9CFQDZ336ljqnHcaFg-LWy89Nc'
+BOT_TOKEN = '8462558990:AAGwph1KVBRIJw7ZTJ3cLJm0Qp3lvmIlO6I'
 ADMIN_ID = [7681846627]
 CHECKER_API_URL = 'https://brainaiapi.up.railway.app/shopify_parallel'
 
